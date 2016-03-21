09a7ebac359a86d9f165dc40bcf329c2ce670884
Sean Johnson <hello@seanjohnson.nz>
2016-03-21 20:33:44 +1300

    uploading last challenge

---
+++ local-scope-and-functions.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  'use strict';
+  var myVar = 10; 
+  
+  console.log(myVar);
+}
+myFunction();
+
+// run and check the console 
+// myVar is not defined outside of myFunction
+
+
+// now remove the console log line to pass the test
+
